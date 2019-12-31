04c42e6ad58f740698365a65bd885efa82ed8640
Vinaria <47085257+Vinaria@users.noreply.github.com>
2019-12-31 14:33:12 +0300

    Теперь не придутся восстанавливать систему регистрации

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,28 @@ from flask import render_template, request, send_from_directory
 from app import app
 from app.pages_data import *
 from app.answers_data import *
+import json
+import os
 
-tasks_amount = int(0)
+tasks_amount = 0
+
+
+def digit(s):
+    s1 = ''
+    for i in range(len(s)):
+        if s[i].isdigit() or s[i] == '.':
+            s1 += s[i]
+    return s1
+
+
+def exists(log, pas):
+    with open('app/person_data.json', 'r', encoding='utf-8') as fh:
+        data = json.load(fh)
+
+    if (log in data) and (pas in data[log['password']]):
+        return True
+    else:
+        return False
 
 
 @app.route('/')
@@ -29,22 +49,43 @@ def get_images(file):
 def answer_check(page_no):
     global tasks_amount
     answer = request.form.get('answer')
+    answer = digit(answer)
 
     if answer == answers[page_no]:
         tasks_amount += 1
-        return render_template('result.html', check_result=True, tasks_amount = tasks_amount, page=pages[page_no])
+        return render_template('result.html', check_result=True, tasks_amount=tasks_amount, page=pages[page_no], answered=True)
     else:
-        return render_template('result.html', check_result=False, tasks_amount = tasks_amount, page=pages[page_no])
+        return render_template('result.html', check_result=False, tasks_amount=tasks_amount, page=pages[page_no])
 
 
-@app.route('/sign/<to_do>')
-def sign_up(to_do):
-    return render_template('account.html', to_do=to_do)
+@app.route('/signup', methods=['POST', 'GET'])
+def registration():
+    login = request.form.get('log')
+    pas1 = request.form.get('pas1')
+    pas2 = request.form.get('pas2')
+
+    with open('app.person_data.json', 'r', encoding='utf-8') as fh:
+        data = json.load(fh)
+
+    if pas1 == pas2 and login not in data:
+        data.add(login['password'])
+        data[login['password']] = pas1
+
+        with open('person_data.json', 'w', encoding='utf-8') as fh:
+            fh.write(json.dumps(data, ensure_ascii=False))
+
+        return render_template('regist_result.html', result=True)
+    else:
+        return render_template('regist_result.html', result=False)
 
 
+@app.route('/signin', methods=['POST', 'GET'])
+def sign_in():
+    login = request.form.get('log')
+    pas = request.form.get('pas')
 
+    if exists(login, pas):
 
-
-
-
-
+        return render_template('index.html', registered=True)
+    else:
+        return render_template('cant_sign_in.html')
